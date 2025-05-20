e21872de9c5bb44b26fefd1a4ea7264e4a44ab7c
Albin Kjellson <albinkj91@yahoo.com>
2025-05-20 22:41:50 +0200

    Finished 3sum

--- 3sum/main.cc
+++ 3sum/main.cc
@@ -2,14 +2,62 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <string>
+#include <sstream>
+#include <unordered_set>
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int>& nums)
 {
-	return vector<vector<int>>{};
+	vector<vector<int>> triplets{};
+	sort(nums.begin(), nums.end());
+
+	for(size_t i{}; i < nums.size(); ++i)
+	{
+		if(nums.at(i) > 0)
+			break;
+		if(i > 0 && nums.at(i) == nums.at(i - 1))
+			continue;
+
+		size_t j{i+1}, k{nums.size()-1};
+		while(j < k)
+		{
+			int sum = nums.at(i) + nums.at(j) + nums.at(k);
+			if(sum == 0)
+			{
+				triplets.push_back({nums.at(i), nums.at(j), nums.at(k)});
+				++j;
+				--k;
+				while(j < k && nums.at(j) == nums.at(j-1))
+					++j;
+			}
+			else if(sum < 0) // Increment middle pointer
+			{
+				++j;
+			}
+			else if(sum > 0) // Decrement end pointer
+			{
+				--k;
+			}
+		}
+	}
+	return triplets;
 }
 
 int main()
 {
+	vector<int> nums{-1,0,1,2,-1,-4};
+	vector<vector<int>> triplets{threeSum(nums)};
 
+	cout << '[';
+	for(size_t i{}; i < triplets.size(); ++i)
+	{
+		cout << '[';
+		for(size_t j{}; j < triplets.at(i).size(); ++j)
+		{
+			cout << triplets.at(i).at(j) << ',';
+		}
+		cout << "],";
+	}
+	cout << ']' << endl;
 }
---
+++ car-fleet/main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int carFleet(int target, vector<int>& position, vector<int>& speed) {
+	return 0;
+}
+
+int main()
+{
+	int target{12};
+	vector<int> position{10,8,0,5,3};
+	vector<int> speed{2,4,1,1,3};
+	carFleet(target, position, speed);
+}
