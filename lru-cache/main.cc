ac1c7b3cc6e72ffc09d6168d63b2dfeff69c7e1d
Albin Kjellson <albinkj91@yahoo.com>
2025-07-09 20:06:12 +0200

    LRU cache

--- lru-cache/main.cc
+++ lru-cache/main.cc
@@ -1,21 +1,8 @@
 #include <iostream>
 #include <unordered_map>
+#include <list>
+#include <iterator>
 using namespace std;
-
-class Node
-{
-public:
-    Node(int const val)
-        :val{val}
-    { }
-    Node()
-    { }
-
-    Node* next{nullptr};
-    Node* prev{nullptr};
-    int val;
-    int key;
-};
 
 class LRUCache
 {
@@ -28,115 +15,44 @@ public:
     {
         if(map.find(key) != map.end())
         {
-            Node* tmp{map.at(key)};
-            if(tmp->prev != nullptr)
-            {
-                tmp->prev->next = tmp->next;
-                tmp->next->prev = tmp->prev;
-                tmp->next = nullptr;
-                tail->next = tmp;
-                tmp->prev = tail;
-                tail = tail->next;
-            }
-            else if(tmp->next != nullptr)
-            {
-                head = tmp->next;
-                tail->next = tmp;
-                tmp->prev = tail;
-                tail = tail->next;
-                tail->next = nullptr;
-            }
-            return tmp->val;
+            values.erase(map.at(key).second);
+            values.push_back(key);
+            auto test{--values.end()};
+            map.at(key).second = test;
+            return map.at(key).first;
         }
         return -1;
     }
     
     void put(int key, int value)
     {
-        Node* node{new Node{value}};
-        node->key = key;
-        if(head == nullptr)
+        if(map.find(key) != map.end())
         {
-            head = node;
-            tail = node;
-            --size;
+            values.erase(map.at(key).second);
+            map.erase(key);
         }
-        else if(size > 0)
+        else if(static_cast<int>(map.size()) == size)
         {
-            if(map.find(key) == map.end())
-            {
-                tail->next = node;
-                node->prev = tail;
-                tail = tail->next;
-            }
-            else
-            {
-                Node* tmp{map.at(key)};
-                map.at(key) = node;
-                if(tmp->prev != nullptr)
-                {
-                    tmp->prev->next = tmp->next;
-                    tmp->next->prev = tmp->prev;
-                    tail->next = node;
-                    tail = tail->next;
-                }
-                else
-                {
-                    head = node;
-                }
-            }
-            --size;
+            int lru{values.front()};
+            values.pop_front();
+            map.erase(lru);
         }
-        else
-        {
-            if(map.find(key) != map.end())
-            {
-                auto pos = map.find(key);
-                map.erase(pos);
-            }
-            else
-            {
-                auto pos = map.find(head->key);
-                map.erase(pos);
-            }
-            if(head->next != nullptr)
-                head = head->next;
-            if(head->prev != nullptr)
-                head->prev = nullptr;
-            tail->next = node;
-            tail = tail->next;
-        }
-        map.emplace(key, node);
-    }
-
-    void print()
-    {
-        Node* tmp{head};
-        while(tmp != nullptr)
-        {
-            cout << tmp->val << ',';
-            tmp = tmp->next;
-        }
-        cout << endl;
+        values.push_back(key);
+        map.emplace(key, pair<int, list<int>::iterator>{value, --values.end()});
     }
 private:
-    Node* head{new Node{}};
-    Node* tail{new Node{}};
-    unordered_map<int, Node*> map{};
+    list<int> values{};
+    unordered_map<int, pair<int, list<int>::iterator>> map{};
     int size;
 };
 
 int main()
 {
-    LRUCache cache{2};
-    cache.put(1,1);
-    cache.put(2,2);
-    cache.get(1);
-    cache.put(3,3);
-    cache.get(2);
-    cache.put(4,4);
-    cache.get(1);
-    cache.get(3);
-    cache.get(4);
-    //cache.print();
+    LRUCache cache2{2};
+    cache2.put(2,1);
+    cache2.put(2,2);
+    cout << cache2.get(2) << endl;
+    cache2.put(1,1);
+    cache2.put(4,1);
+    cout << cache2.get(2) << endl;
 }
